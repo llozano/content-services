5f16c2573452c97159d820ee3111e9af647f9079
Leonardo Lozano <lozano.leonardo@gmail.com>
2021-01-28 23:18:56 -0800

    MICRO-001 - Microservice. - WIP

---
+++ src/main/java/com/interview/contentservices/ContentservicesApplication.java
@@ -0,0 +1,13 @@
+package com.interview.contentservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentservicesApplication.class, args);
+	}
+
+}
